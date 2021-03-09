7e03f3ce6b85207ea489d5050efd9ff198e32f75
RRoLL545 <motom@yandex.ru>
2021-03-09 11:13:32 +0400

    feat: creat js functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,57 @@
 
 exports.min = function min (array) {
-  return 0;
+    if ( array === undefined ) {
+        return 0;
+    }
+
+    if ( array.length === 0 ) {
+        return 0;
+    }
+        
+    let tempMin = array[0];
+    
+    for ( let i = 1; i < array.length; i++) {
+        if ( array[i] < tempMin ) {
+            tempMin = array[i];
+        }
+    }
+      
+    return tempMin;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if ( array === undefined ) {
+        return 0;
+    }
+
+    if ( array.length === 0 ) {
+        return 0;
+    }
+      
+    let tempMax = array[0];
+      
+    for ( let i = 1; i < array.length; i++) {
+        if ( array[i] > tempMax ) {
+            tempMax = array[i];
+        }
+    }
+      
+    return tempMax;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if ( array === undefined ) {
+        return 0;
+    }
+
+    if ( array.length === 0 ) {
+        return 0;
+    }
+    
+    let sum = 0;
+    for ( let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    
+    return sum / array.length;
 }
